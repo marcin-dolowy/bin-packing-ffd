3fdbe94776e1c28810376bd07006116ea6aa1cb1
Marcin Dołowy <marcindolowy00@gmail.com>
2024-05-13 18:01:48 +0200

    average count time and memory for branch and bound

--- src/main/java/org/example/MainBaD.java
+++ src/main/java/org/example/MainBaD.java
@@ -6,40 +6,52 @@ import java.io.PrintWriter;
 import java.util.Random;
 
 public class MainBaD {
-    private static final int BIN_CAPACITY = 10;
-    private static final int MIN_ELEMENTS = 5;
-    private static final int MAX_ELEMENTS = 21;
-    private static final int STEP = 1;
+  private static final int BIN_CAPACITY = 10;
+  private static final int MIN_ELEMENTS = 5;
+  private static final int MAX_ELEMENTS = 21;
+  private static final int STEP = 1;
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        BinPacking binPacking = new BinPacking();
-        Random random = new Random();
+    BinPacking binPacking = new BinPacking();
+    Random random = new Random();
 
-        try (PrintWriter writer = new PrintWriter(new FileWriter("output_branch_and_bound.txt"))) {
-            writer.println("number_of_elements;time_taken;memory_used");
-            for (int i = MIN_ELEMENTS; i <= MAX_ELEMENTS; i += STEP) {
-                int[] weights = random.ints(i, 1, 10).toArray();
+    try (PrintWriter writer = new PrintWriter(new FileWriter("output_branch_and_bound.txt"))) {
+      writer.println("number_of_elements;time_taken;memory_used");
+      for (int i = MIN_ELEMENTS; i <= MAX_ELEMENTS; i += STEP) {
 
-                Runtime runtime = Runtime.getRuntime();
-                runtime.gc();
-                long memoryBefore = runtime.totalMemory() - runtime.freeMemory();
+        long averageMemoryUsed = 0;
+        long averageDuration = 0;
 
-                long startTime = System.nanoTime();
-                binPacking.branchAndBound(weights, BIN_CAPACITY);
-                long endTime = System.nanoTime();
+        for (int j = 0; j < 100; j++) {
 
-                long memoryAfter = runtime.totalMemory() - runtime.freeMemory();
+          int[] weights = random.ints(i, 1, 10).toArray();
 
-                long duration = endTime - startTime;
-                long memoryUsed = memoryAfter - memoryBefore;
+          Runtime runtime = Runtime.getRuntime();
+          runtime.gc();
+          long memoryBefore = runtime.totalMemory() - runtime.freeMemory();
 
-                System.out.println(i + ";" + duration + ";" + memoryUsed);
-                writer.println(i + ";" + duration + ";" + memoryUsed);
-            }
-        } catch (IOException e) {
-            System.out.println("Error writing to file: " + e.getMessage());
+          long startTime = System.nanoTime();
+          binPacking.branchAndBound(weights, BIN_CAPACITY);
+          long endTime = System.nanoTime();
+
+          long memoryAfter = runtime.totalMemory() - runtime.freeMemory();
+
+          long duration = endTime - startTime;
+          long memoryUsed = memoryAfter - memoryBefore;
+
+          averageDuration += duration;
+          averageMemoryUsed += memoryUsed;
         }
-    }
 
+        averageDuration /= 100;
+        averageMemoryUsed /= 100;
+
+        System.out.println(i + ";" + averageDuration + ";" + averageMemoryUsed);
+        writer.println(i + ";" + averageDuration + ";" + averageMemoryUsed);
+      }
+    } catch (IOException e) {
+      System.out.println("Error writing to file: " + e.getMessage());
+    }
+  }
 }
